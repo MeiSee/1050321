c8a9f65ff6b5f1d9e9d82e0418f002df988da625
MeiSee <meisee1603@hotmail.com>
2016-03-24 01:19:56 +0800

    update and add

---
+++ q3.py
@@ -0,0 +1,13 @@
+while True:
+    score = int(input("請輸入分數："))
+    if score <=49:
+        print("E")
+    elif 50 <=score <=59:
+        print("D")
+    elif 60 <=score <=69:
+        print("C")
+    elif 70 <=score <=79:
+        print("B")
+    elif 80<=score <=100:
+        print("A")
+    
